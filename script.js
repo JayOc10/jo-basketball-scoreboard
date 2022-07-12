841fd157a3f193fa5abc0a04a2d88e58f5ee2ecf
Jeannette Torres <jeannette.ocegueda@yahoo.com>
2022-07-11 19:26:32 -0700

    added code to js

---
+++ script.js
@@ -0,0 +1,31 @@
+let count = 0
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+
+
+function homeBtn1(){
+    count = count + 1
+    homeScore.textContent = count
+}
+function homeBtn2(){
+    count = count + 2
+    homeScore.textContent = count
+}
+function homeBtn3(){
+    count = count + 3
+    homeScore.textContent = count
+}
+
+function guestBtn1(){
+    count = count + 1
+    guestScore.textContent = count
+}
+function guestBtn2(){
+    count = count + 2
+    guestScore.textContent = count
+}
+function guestBtn3(){
+    count = count + 3
+    guestScore.textContent = count
+}
